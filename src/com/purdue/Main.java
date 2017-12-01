1d84e31e190f8e9aa421383d8eba3a05cecee965
Luke <radiotech36@gmail.com>
2017-12-01 00:00:25 -0500

    Added Client Class

--- src/com/purdue/Main.java
+++ src/com/purdue/Main.java
@@ -2,7 +2,11 @@ package com.purdue;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        Client p1 = new Client("Adam", "Sullivan", 34, "ldsakfhads;ih", "Kokomo",
+                "M", "IN", 46902, 58470494, "7654873894", "adshfasklhflkads@gmail.com",
+                001, true, "read");
+        System.out.println(p1);
     }
 }
